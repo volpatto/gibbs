33ddb344f9c02ea87e6f02e04bea68c492d1fcfb
volpatto <volpatto@lncc.br>
2019-05-17 23:15:00 -0300

    Improve stability analysis a little more

--- gibbs/stability_analysis.py
+++ gibbs/stability_analysis.py
@@ -18,10 +18,9 @@ def stability_test(model, P, T, z, monitor=False):
     result = de(
         _reduced_tpd, 
         bounds=search_space, 
-        args=[model, P, T, z, f_z],
-        popsize=10,
+        args=[model, P, T, f_z],
+        popsize=25,
         recombination=0.9,
-        maxiter=50,
         disp=monitor,
         polish=False
     )
@@ -29,7 +28,7 @@ def stability_test(model, P, T, z, monitor=False):
     x = result.x / result.x.sum()
     reduced_tpd = result.fun
 
-    if np.abs(reduced_tpd) <= 1e-7 and np.allclose(x, z):
+    if np.allclose(x, z):
         phase_split = False
     elif reduced_tpd < 0.:
         phase_split = True
@@ -45,21 +44,16 @@ def stability_test(model, P, T, z, monitor=False):
     return stability_test_result
 
 
-def _reduced_tpd(x, model, P, T, z, f_z):
-    tol = 1e-2
-    x = x / x.sum()
-    condition1 = 1 - tol <= x.sum()
-    condition2 = x.sum() <= 1 + tol
-    condition3 = x.max() <= 1.0
-    condition4 = x.min() >= 0.0
-    feasible_conditions = condition1 and condition2 and condition3 and condition4
+def _reduced_tpd(x, model, P, T, f_z):
+    tol = 1e-3
+    # x = x / x.sum()  # this probably could be removed
 
     f_x = model.fugacity(P, T, x)
-    tpd = np.dot(x, (np.log(f_x) - np.log(f_z)))
+    tpd = np.sum(x * (np.log(f_x / f_z)))
 
-    if not feasible_conditions:
+    if not 1 - tol <= np.sum(x) <= 1 + tol:
         # Penalization if candidate is not in feasible space
-        penalty_parameter = 10.
+        penalty_parameter = 1e3
         tpd += penalty_parameter * _penalty_function(x, 1)
 
     return tpd
