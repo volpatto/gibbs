6f83719a81c678e5341de5dd5e64305223d680df
volpatto <volpatto@lncc.br>
2019-05-31 00:15:25 -0300

    Increase the mutation rate, decrease recombination

--- gibbs/stability_analysis.py
+++ gibbs/stability_analysis.py
@@ -11,8 +11,8 @@ class StabilityResult:
 
 
 def stability_test(
-        model, P, T, z, strategy='best1bin', popsize=30, recombination=0.95, mutation=0.6,
-        tol=1e-3, rtol=1e-3, seed=np.random.RandomState(), workers=-1, monitor=False, polish=True
+        model, P, T, z, strategy='best1bin', popsize=30, recombination=0.85, mutation=0.7,
+        tol=1e-2, rtol=1e-3, seed=np.random.RandomState(), workers=-1, monitor=False, polish=True
 ):
     if popsize <= 0:
         raise ValueError('Number of individuals must be greater than 0.')
