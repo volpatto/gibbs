51a0650b191b310f9df060e1f4fdcd9735e5ce77
volpatto <volpatto@lncc.br>
2019-05-30 23:12:07 -0300

    Improve stability test

--- gibbs/stability_analysis.py
+++ gibbs/stability_analysis.py
@@ -10,7 +10,32 @@ class StabilityResult:
     reduced_tpd: float
 
 
-def stability_test(model, P, T, z, monitor=False, rtol=1e-3, polish=True):
+def stability_test(
+        model, P, T, z, strategy='best1bin', popsize=25, recombination=0.95, mutation=0.5,
+        tol=1e-2, rtol=1e-3, seed=np.random.RandomState(), workers=-1, monitor=False, polish=True
+):
+    if popsize <= 0:
+        raise ValueError('Number of individuals must be greater than 0.')
+    if type(popsize) != int:
+        raise TypeError('Population size must be an integer number.')
+    if not 0 < recombination <= 1:
+        raise ValueError('Recombination must be a value between 0 and 1.')
+    if type(mutation) == tuple:
+        mutation_dithering_array = np.array(mutation)
+        if len(mutation) > 2:
+            raise ValueError('Mutation can be a tuple with two numbers, not more.')
+        if mutation_dithering_array.min() < 0 or mutation_dithering_array.max() > 2:
+            raise ValueError('Mutation must be floats between 0 and 2.')
+        elif mutation_dithering_array.min() == mutation_dithering_array.max():
+            raise ValueError("Values for mutation dithering can't be equal.")
+    else:
+        if type(mutation) != int and type(mutation) != float:
+            raise TypeError('When mutation is provided as a single number, it must be a float or an int.')
+        if not 0 < mutation < 2:
+            raise ValueError('Mutation must be a number between 0 and 2.')
+    if tol < 0 or rtol < 0:
+        raise ValueError('Tolerance must be a positive float.')
+
     n_components = model.number_of_components
     search_space = [(0, 1)] * n_components
     f_z = model.fugacity(P, T, z)
@@ -19,12 +44,15 @@ def stability_test(model, P, T, z, monitor=False, rtol=1e-3, polish=True):
         _reduced_tpd,
         bounds=search_space, 
         args=[model, P, T, f_z],
-        popsize=30,
-        recombination=0.95,
-        mutation=0.6,
-        tol=1e-3,
+        strategy=strategy,
+        popsize=popsize,
+        recombination=recombination,
+        mutation=mutation,
+        tol=tol,
         disp=monitor,
-        polish=polish
+        polish=polish,
+        seed=seed,
+        workers=workers
     )
 
     x = result.x / result.x.sum()
@@ -33,10 +61,10 @@ def stability_test(model, P, T, z, monitor=False, rtol=1e-3, polish=True):
     if np.allclose(x, z, rtol=rtol):  # This criterion could be separated in a function itself
         # with a norm
         phase_split = False
-    elif reduced_tpd < 0.:
-        phase_split = True
     elif np.abs(reduced_tpd) < 1e-10:
         phase_split = False
+    elif reduced_tpd < 0.:
+        phase_split = True
     else:
         phase_split = False
 
